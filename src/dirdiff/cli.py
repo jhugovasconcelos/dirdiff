d57ba77c1c6f2a249a14052219aee0d9913791c2
Joao Hugo Vasconcelos <jhugovasconcelos93@gmail.com>
2025-06-17 14:43:00 -0300

    Fixing pyproject.toml and removing main module

--- src/dirdiff/cli.py
+++ src/dirdiff/cli.py
@@ -9,5 +9,5 @@ path=click.Path(exists=True, dir_okay=True, )
 @click.version_option(VERSION, message="%(version)s")
 @click.argument('source', type=path)
 @click.argument('dest', type=path)
-def diffdir(source, dest):
+def cli(source, dest):
   show_diff(source, dest)
--- src/dirdiff/main.py
+++
@@ -1,4 +0,0 @@
-from dirdiff.app import diffdir
-
-if __name__ == '__main__':
-    diffdir()
