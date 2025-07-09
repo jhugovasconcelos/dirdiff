fffad80968a6adc13bb17554b32f752b30ad00e6
Joao Hugo Vasconcelos <jhugovasconcelos93@gmail.com>
2025-07-08 21:51:08 -0300

    Changing logic to show three columns

--- src/dirdiff/logic.py
+++ src/dirdiff/logic.py
@@ -2,79 +2,48 @@ from dirdiff.rich_imports import log, console, table, equal, different
 from dirdiff.directories import get_files_in_dir
 
 
-def better_zip(l1:list, l2:list) -> list[tuple]:
-  """
-    This function takes two lists of different sizes and where some or all elements are different and equalizes them by 
-    putting blank spaces in between them until both have the same size and all the elements that are equal are in the
-    same index on both lists and the ones that are different are in the same index as blank spaces on the other list. 
-    Then it returns a list with tuples containing all the elements in both lists, where equal elements are in the same 
-    tuple and unique elements are grouped with blank spaces.
-
-    Remember to pass the bigger list as l1 and the smaller list as l2. 
-    Will fix that in the future
-  """
-  zipped = []
-  l1 = sorted(l1)
-  l2 = sorted(l2)
-
-  # Main loop iterating over the lenght of the bigger list (that is decided outside the function call)
-  for i in range(len(l1)):
-    # First, check if the smaller list is exhausted
-    if i < len(l2):
-        # Are they the same? Done
-        if l1[i] == l2[i]:
-          pass
-        else:
-          # Is either of them blank? Done
-          if l1[i] == " " or l2[i] == " ":
-            pass
-          # Here is the actual shift
-          else:
-            if l1[i] < l2[i]:
-              if i == 0:
-                l2 = [" "] + l2
-              else:
-                l2 = l2[:i] + [" "] + l2[i:]
-            else:
-              if i == 0:
-                l1 = [" "] + l1
-              else:
-                l1 = l1[:i-1] + [" "] + l1[i:]
+def show_diff(base_directory:str, compare_directory:str):
+  l1 = get_files_in_dir(base_directory)
+  l2 = get_files_in_dir(compare_directory)
   
-  # Zipping it all together
-  for i in range(len(l1)):
-    zipped.append((l1[i], l2[i]))
-  
-  return zipped
+  common = sorted([u for u in l1 if u in l2])
+  unique_l1 = sorted([u for u in l1 if u not in l2])
+  unique_l2 = sorted([u for u in l2 if u not in l1])
 
-def compare_dirs(src:str, dst:str):
-  log.debug(f"Source Directory: {src}")
-  log.debug(f"Destination Directory: {dst}")
+  len_common = len(common)
+  len_u1 = len(unique_l1)
+  len_u2 = len(unique_l2)
+  longest = max([len_common, len_u1, len_u2])
 
-  src_files = get_files_in_dir(src) 
-  dst_files = get_files_in_dir(dst)
+  if len_common == longest:
+    unique_l1.extend([" " for _ in range(len_common-len_u1)])
+    unique_l2.extend([" " for _ in range(len_common-len_u2)])
+  elif len_u1 == longest:
+    unique_l2.extend([" " for _ in range(len_u1-len_u2)])
+    common.extend([" " for _ in range(len_u1-len_common)])
+  elif len_u2 == longest:
+    unique_l1.extend([" " for _ in range(len_u2-len_u1)])
+    common.extend([" " for _ in range(len_u2-len_common)])
 
-  if src_files > dst_files:
-    table_rows = better_zip(l1=src_files, l2=dst_files)
+  _table = [files for files in zip(unique_l1, common, unique_l2)]
+  if unique_l1 or unique_l2:
+    # Creating Table columns
+    table.add_column(header=f'Only in {base_directory}', header_style="red", justify="center", style=different)
+    table.add_column(header=f'Common files', header_style="green", justify="center", style=equal)
+    table.add_column(header=f'Only in {compare_directory}', header_style="red", justify="center", style=different)
+
+    # Creating Table rows
+    table_rows = _table
+    for row in table_rows:
+        file_base, file_common, file_comp = row
+        table.add_row(file_base, file_common, file_comp)
   else:
-    table_rows = better_zip(l1=dst_files, l2=src_files)
-
-  return table_rows
-
-def show_diff(src:str, dst:str):
-  log.debug("Calling functions")
-   
-  # Creating Table columns
-  table.add_column(header=f'{src}', header_style="cyan", justify="center")
-  table.add_column(header=f'{dst}', header_style="green", justify="center")
+    # Creating Table columns
+    table.add_column(header=f'Directories are equivalent', header_style="green", justify="center", style=equal)
+    # Creating Table rows
+    table_rows = _table
+    for row in table_rows:
+        file_base, file_common, file_comp = row
+        table.add_row(file_base, file_common, file_comp)
   
-  # Creating Table rows
-  table_rows = compare_dirs(src, dst)
-  for row in table_rows:
-      file_src, file_dst = row
-      if file_src == " " or file_dst == " ":
-        table.add_row(file_src, file_dst, style=different)
-      else:
-        table.add_row(file_src, file_dst, style=equal)
-
   console.print(table)
